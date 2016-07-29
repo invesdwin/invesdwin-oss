2d9a9d0271e11fae3a9204e60ee45a1b4557d960
subes <gsubes@gmail.com>
2016-07-29 13:27:13 +0100

    jenkins script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-bom/invesdwin-bom/pom.xml
-mvn clean deploy -DskipTests -U -N -e -f invesdwin-maven-plugin/invesdwin-maven-plugin-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context/invesdwin-context-parent/pom.xml
+mvn clean deploy -DskipTests -T1C -U -e
