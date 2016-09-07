e7ab8249340062cdfecff47f38a19fd4caedb622
subes <gsubes@gmail.com>
2016-09-07 22:51:16 +0200

    fix stash check

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # invesdwin-oss
 This is just a simplification for the continuous integration setup. Also to provide an easier way to checkout all these projects at once. Use the following command to keep your checkout updated (always following master head):
 
-    sh pull.sh
+    bash pull.sh
     
 Note that you can also put this into your .mrconfig to let [myrepos](https://myrepos.branchable.com/) handle updates properly for you.
 
--- pull.sh
+++ pull.sh
@@ -3,16 +3,29 @@
 git pull
 git submodule update --init --recursive --remote
 
-for dir in ./*
+for dir in *
 do
   test -d "$dir" || continue
   cd $dir
-  git stash
-  STASHED=$?
-  git checkout master
-  git pull
-  if [ $STASHED == 0 ] 
+if [[ `git status --porcelain` ]]; then
+  # changes
+  LOCALCHANGES=1
+else
+  # no changes
+  LOCALCHANGES=0
+fi
+  if [ $LOCALCHANGES == 1 ]
   then
+    echo -- $dir -- stashing
+    git stash
+  fi
+  echo -- $dir -- checking out master
+  git checkout master
+  echo -- $dir -- pulling
+  git pull
+  if [ $LOCALCHANGES == 1 ]
+  then
+    echo -- $dir -- popping stash
     git stash pop
   fi
   cd ..
