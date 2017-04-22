d035ee7a7a8f74b940f160765b80858243b9e5f2
subes <gsubes@gmail.com>
2017-04-22 19:53:58 +0200

    integration submodule added

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,7 @@ set -e
 cd "$(dirname "$0")"
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-bom/invesdwin-bom/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context/invesdwin-context-parent/pom.xml
+mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-integration/invesdwin-context-integration-parent/pom.xml
+mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-persistence/invesdwin-context-persistence-parent/pom.xml
+mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-security/invesdwin-context-security-parent/pom.xml
 mvn clean deploy -DskipTests -T1C -U -e
