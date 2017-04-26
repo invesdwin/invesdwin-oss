596deced50a8eaeeef7337c250ff0af36a570911
subes <gsubes@gmail.com>
2017-04-26 11:15:38 +0200

    rintegrate submodule

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ mvn clean deploy -DskipTests -U -N -e -f invesdwin-context/invesdwin-context-par
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-integration/invesdwin-context-integration-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-persistence/invesdwin-context-persistence-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-security/invesdwin-context-security-parent/pom.xml
+mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-rintegrate/invesdwin-context-rintegrate-parent/pom.xml
 mvn clean deploy -DskipTests -T1C -U -e
