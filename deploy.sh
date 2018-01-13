13ac7227bff947d1c67f9a84766d808c5a0765e5
subes <gsubes@gmail.com>
2018-01-13 18:26:41 +0100

    fix order

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 set -e
 cd "$(dirname "$0")"
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-bom/invesdwin-bom/pom.xml
+mvn clean deploy -DskipTests -T1C -U -e -f invesdwin-maven-plugin/invesdwin-maven-plugin-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context/invesdwin-context-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-integration/invesdwin-context-integration-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-persistence/invesdwin-context-persistence-parent/pom.xml
@@ -11,5 +12,4 @@ mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-python/invesdwin-cont
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-matlab/invesdwin-context-matlab-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-client/invesdwin-context-client-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-webproxy/invesdwin-webproxy-parent/pom.xml
-mvn clean deploy -DskipTests -T1C -U -e -f invesdwin-maven-plugin/invesdwin-maven-plugin-parent/pom.xml
 mvn clean deploy -DskipTests -T1C -U -e
