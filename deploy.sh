d1a89b54fd67ecfe3575b3aed14fee7a82c66edf
subes <gsubes@gmail.com>
2018-01-13 18:47:56 +0100

    deploy nowicket-parent pom

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ set -e
 cd "$(dirname "$0")"
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-bom/invesdwin-bom/pom.xml
 mvn clean deploy -DskipTests -T1C -U -e -f invesdwin-maven-plugin/invesdwin-maven-plugin-parent/pom.xml
+mvn clean deploy -DskipTests -U -N -e -f invesdwin-nowicket/invesdwin-nowicket-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context/invesdwin-context-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-integration/invesdwin-context-integration-parent/pom.xml
 mvn clean deploy -DskipTests -U -N -e -f invesdwin-context-persistence/invesdwin-context-persistence-parent/pom.xml
