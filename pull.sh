ac4064274f049a71e5b04188f30f8b3f4cf39718
Edwin Stang <gsubes@gmail.com>
2022-07-03 11:17:04 +0200

    change init command

--- pull.sh
+++ pull.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
 git pull
-git submodule update --init --recursive --remote
+#https://stackoverflow.com/questions/10168449/git-update-submodules-recursively
+git submodule init
+git submodule update
+git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git submodule update --recursive'
+
 
 for dir in *
 do
