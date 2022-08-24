165908f15f43db922c4085083346e33d2e7253d9
Edwin Stang <gsubes@gmail.com>
2022-08-24 20:49:53 +0200

    checkout_date.sh

---
+++ checkout_date.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+cd "$(dirname "$0")"
+
+#DATE='master@{2022-08-01 00:00:00}'
+DATE=$1
+
+for dir in *
+do
+  test "dependencies" = "$dir" && continue
+  test -d "$dir" || continue
+  echo -- $dir
+  cd $dir
+if [[ `git status --porcelain` ]]; then
+  # changes
+  LOCALCHANGES=1
+else
+  # no changes
+  LOCALCHANGES=0
+fi
+  if [ $LOCALCHANGES == 1 ]
+  then
+    echo -- $dir -- stashing
+    git stash
+  fi
+  echo -- $dir -- checking out master@{$DATE}
+  git checkout "master@{$DATE}"
+  echo -- $dir -- pulling
+  git pull
+  if [ $LOCALCHANGES == 1 ]
+  then
+    echo -- $dir -- popping stash
+    git stash pop
+  fi
+  cd ..
+done
