dd8db74529d9583372d359b58602d4db294319d3
Edwin Stang <gsubes@gmail.com>
2024-10-20 15:07:04 +0200

    migrate scripting integrations into consolidated repository invesdwin-scripting

--- test.sh
+++ test.sh
@@ -16,14 +16,10 @@ echo "-- invesdwin-context-persistence-parent"
 mvn -B -Dmaven.repo.local=$REPO clean install -DskipTests -N -e -f invesdwin-context-persistence/invesdwin-context-persistence-parent/pom.xml
 echo "-- invesdwin-context-security-parent"
 mvn -B -Dmaven.repo.local=$REPO clean install -DskipTests -N -e -f invesdwin-context-security/invesdwin-context-security-parent/pom.xml
-echo "-- invesdwin-context-r-parent"
-mvn -B -Dmaven.repo.local=$REPO clean install -DskipTests -N -e -f invesdwin-context-r/invesdwin-context-r-parent/pom.xml
-echo "-- invesdwin-context-python-parent"
-mvn -B -Dmaven.repo.local=$REPO clean install -DskipTests -N -e -f invesdwin-context-python/invesdwin-context-python-parent/pom.xml
-echo "-- invesdwin-context-matlab-parent"
-mvn -B -Dmaven.repo.local=$REPO clean install -DskipTests -N -e -f invesdwin-context-matlab/invesdwin-context-matlab-parent/pom.xml
 echo "-- invesdwin-context-client-parent"
 mvn -B -Dmaven.repo.local=$REPO clean install -DskipTests -N -e -f invesdwin-context-client/invesdwin-context-client-parent/pom.xml
+echo "-- invesdwin-scripting-parent"
+mvn -B -Dmaven.repo.local=$REPO clean install -DskipTests -N -e -f invesdwin-scripting/invesdwin-scripting-parent/pom.xml
 echo "-- invesdwin-webproxy-parent"
 mvn -B -Dmaven.repo.local=$REPO clean install -DskipTests -N -e -f invesdwin-webproxy/invesdwin-webproxy-parent/pom.xml
 echo "-- build all"
